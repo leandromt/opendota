b9a55fc7ca25ff4257e569089b49e27f751064c4
Leandro Tavares <leandro.mendestavares@gmail.com>
2018-05-01 12:25:44 -0300

    Add Imgs

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@
     <ul class="lista-heroes">
       <li v-for="hero of heroes" v-bind:key="hero.id">
         <div class="painel-hero">
+          <img class="img-responsiva" :src="heroImg(hero.id)" :alt="hero.localized_name">
           <p><strong>ID:</strong> <span>{{ hero.id }}</span></p>
-          <p><strong>Name:</strong> <span>{{ hero.name }}</span></p>
-          <p><strong>Name 2:</strong> <span>{{ hero.localized_name }}</span></p>
+          <p><strong>Name:</strong> <span>{{ hero.localized_name }}</span></p>
           <p><strong>Attribute:</strong> <span>{{ hero.primary_attr }}</span></p>
           <p><strong>Attack:</strong> <span>{{ hero.attack_type }}</span></p>
         </div>
@@ -20,20 +20,26 @@ export default {
 
   name: 'app',
 
-  data () {
-    return {
-      titulo: 'All Heroes Dota 2',
-      heroes: []
-    }
-  },
-
-
   created() {
 
     this.$http.get('https://api.opendota.com/api/heroes/')
       .then(res => res.json())
       .then(heroes => this.heroes = heroes, err => console.log(err));
 
+  },
+
+  data () {
+    return {
+      titulo: 'DotA 2 - All Heroes',
+      subtitulo: 'All Heroes',
+      heroes: []
+    }
+  },
+  
+  methods: {
+    heroImg(id){
+      return 'src/assets/heroes/' + id + '.png';
+    }
   }
 
 
@@ -41,20 +47,44 @@ export default {
 </script>
 
 <style scoped>
+
+  * {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
   .centralizado {
     text-align: center;
+    color: crimson;
+    font-size: 2.6em;
+    padding: 1.2em;
   }
+
   li {
     padding: 20px;
     display: inline-block;
-    width: 25%;
+    width: 20%;
   }
+
+  p {
+    
+  }
+  
   .painel-hero {
     padding: 10px;
-    background-color: darkgrey;
+    background-color: #8080805e;
+    transition-property: all;
+    transition-duration: .5s;
+    color: #fff;
   }
-  * {
-    box-sizing: border-box;
+  .painel-hero:hover {
+    background-color: gray;
   }
+
+  .img-responsiva {
+    width: 100%;
+  }
+
+  
 
 </style>
